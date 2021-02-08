387d7af76372c351e99f7261452b95fe7408d3f1
Ryan Kang <rkang626@gmail.com>
2021-02-08 20:23:10 +0000

    remove user routes which arent set up yet

--- server.js
+++ server.js
@@ -33,12 +33,10 @@ app.use(express.static("public"));
 
 // Separated Routes for each Resource
 // Note: Feel free to replace the example routes below with your own
-const usersRoutes = require("./routes/users");
 const storyRoutes = require("./routes/stories");
 
 // Mount all resource routes
 // Note: Feel free to replace the example routes below with your own
-app.use("/api/users", usersRoutes(db));
 app.use("/stories", storyRoutes(db));
 // Note: mount other resources here, using the same pattern above
 
