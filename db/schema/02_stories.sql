7694d299adc56803a9feac3c17cc9e50bd74067f
Ryan Kang <rkang626@gmail.com>
2021-02-08 21:26:00 +0000

    remove themes table

--- db/schema/02_stories.sql
+++ db/schema/02_stories.sql
@@ -3,7 +3,9 @@ DROP TABLE IF EXISTS stories CASCADE;
 CREATE TABLE stories (
   id SERIAL PRIMARY KEY NOT NULL,
   creator_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
-  cover_photo_id INTEGER REFERENCES photos(id) ON DELETE CASCADE,
+  photo_id INTEGER REFERENCES photos(id) ON DELETE CASCADE,
+  animation_id INTEGER REFERENCES animations(id) ON DELETE CASCADE,
+  ambient_sound_id INTEGER REFERENCES ambient_sounds(id) ON DELETE CASCADE,
   status varchar(255) NOT NULL DEFAULT 'pending',
   title TEXT NOT NULL,
   description TEXT NOT NULL,
