b31a18c5e066ef7987467fbbc9432f4fdd1afdf6
Andrew Han <junmenghan@gmail.com>
2021-02-09 20:31:22 +0000

    front page touchups

--- public/scripts/front_page.js
+++ public/scripts/front_page.js
@@ -8,13 +8,9 @@ $(() => {
     $("div.intro_scroll p#title").css("font-size", 3.5/(1 + scrolltotop/windowHeight) + "rem");
     if (scrolltotop >= 800) {
       intro.slideUp();
-    } else {
-      intro.slideDown();
-    }
-
-    if (scrolltotop >= 900) {
       $('header').slideDown();
     } else {
+      intro.slideDown();
       $('header').slideUp();
     }
   })
