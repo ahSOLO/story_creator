c9128a6541c2d15a22cbc6a3aab57733e8567e7d
Andrew Han <junmenghan@gmail.com>
2021-02-09 18:15:50 +0000

    squashed bugs relating to query requests on story view page

--- routes/story_viewer.js
+++ routes/story_viewer.js
@@ -73,15 +73,15 @@ module.exports = (db) => {
     let pendingContributions;
     const queryString1 = `
     SELECT stories.title as title, creators.name as creator, contributors.name as contributor, creator_animations.type as creator_animation, creator_photos.photo_url as creator_photo, stories.first_entry as first_entry, creator_sounds.type as creator_sound, contributions.order_rank as ord, contributor_animations.type as contributor_anim, contributor_photos.photo_url as contributor_photo, contributions.content as contributor_entry, contributor_sounds.type as contributor_sound, contributions.text_position as contributor_position    FROM stories
-    JOIN contributions ON (story_id = stories.id AND contributions.status = 'accepted')
+    LEFT JOIN contributions ON (story_id = stories.id AND contributions.status = 'accepted')
     JOIN users creators ON creator_id = creators.id
-    JOIN users contributors ON contributor_id = contributors.id
+    LEFT JOIN users contributors ON contributor_id = contributors.id
     JOIN photos creator_photos ON creator_photos.id = stories.photo_id
-    JOIN photos contributor_photos ON contributor_photos.id = contributions.photo_id
+    LEFT JOIN photos contributor_photos ON contributor_photos.id = contributions.photo_id
     JOIN animations creator_animations ON creator_animations.id = stories.animation_id
-    JOIN animations contributor_animations ON contributor_animations.id = contributions.animation_id
+    LEFT JOIN animations contributor_animations ON contributor_animations.id = contributions.animation_id
     JOIN ambient_sounds creator_sounds ON creator_sounds.id = stories.ambient_sound_id
-    JOIN ambient_sounds contributor_sounds ON contributor_sounds.id = contributions.ambient_sound_id
+    LEFT JOIN ambient_sounds contributor_sounds ON contributor_sounds.id = contributions.ambient_sound_id
     WHERE stories.id = $1
     ORDER BY ord;`;
     const queryParams = [req.params.storyID]
@@ -92,7 +92,7 @@ module.exports = (db) => {
     JOIN photos ON contributions.photo_id = photos.id
     JOIN upvotes ON upvotes.contribution_id = contributions.id
     WHERE stories.id = $1
-    GROUP BY contributions.content
+    GROUP BY contributions.id
     ORDER BY COUNT(upvotes)
     LIMIT 3;`;
 
