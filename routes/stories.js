1e26771d4b62664393fd92b57c7145004c7a09e7
Andrew Han <junmenghan@gmail.com>
2021-02-09 05:31:00 +0000

    finish linking view_stories page

--- routes/stories.js
+++ routes/stories.js
@@ -20,9 +20,10 @@ module.exports = (db) => {
 
   // render the "View Story" page for story_id
   router.get("/view/:storyID", (req, res) => {
-    const queryString = `
-    SELECT stories.title as title, creators.name as creator, contributors.name as contributor, creator_animations.type as creator_animation, creator_photos.photo_url as creator_photo, stories.first_entry as first_entry, creator_sounds.type as creator_sound, contributions.order_rank as ord, contributor_animations.type as contributor_anim, contributor_photos as contributor_photo, contributions.content as entry, contributor_sounds.type as contributor_sound
-    FROM stories
+    let entries;
+    let pendingContributions;
+    const queryString1 = `
+    SELECT stories.title as title, creators.name as creator, contributors.name as contributor, creator_animations.type as creator_animation, creator_photos.photo_url as creator_photo, stories.first_entry as first_entry, creator_sounds.type as creator_sound, contributions.order_rank as ord, contributor_animations.type as contributor_anim, contributor_photos.photo_url as contributor_photo, contributions.content as contributor_entry, contributor_sounds.type as contributor_sound, contributions.text_position as contributor_position    FROM stories
     JOIN contributions ON (story_id = stories.id AND contributions.status = 'accepted')
     JOIN users creators ON creator_id = creators.id
     JOIN users contributors ON contributor_id = contributors.id
@@ -32,14 +33,28 @@ module.exports = (db) => {
     JOIN animations contributor_animations ON contributor_animations.id = contributions.animation_id
     JOIN ambient_sounds creator_sounds ON creator_sounds.id = stories.ambient_sound_id
     JOIN ambient_sounds contributor_sounds ON contributor_sounds.id = contributions.ambient_sound_id
-    WHERE stories.id = $1;
-    `
+    WHERE stories.id = $1
+    ORDER BY ord;`;
     const queryParams = [req.params.storyID]
+    const queryString2 =`
+    SELECT MIN(photos.photo_url) as photo_url, contributions.content as content
+    FROM stories
+    JOIN contributions ON (stories.id = story_id AND contributions.status = 'pending')
+    JOIN photos ON contributions.photo_id = photos.id
+    JOIN upvotes ON upvotes.contribution_id = contributions.id
+    WHERE stories.id = $1
+    GROUP BY contributions.content
+    ORDER BY COUNT(upvotes)
+    LIMIT 3;`;
 
-    db.query(queryString, queryParams)
+    db.query(queryString1, queryParams)
     .then((data) => {
-      console.log(data.rows);
-      const templateVars = data.rows;
+      entries = data.rows;
+      return db.query(queryString2, queryParams);
+    })
+    .then((data) => {
+      pendingContributions = (data.rows)
+      const templateVars = { entries, pendingContributions, storyID:req.params.storyID };
       res.render("view_story", templateVars);
     })
     .catch(err => {
@@ -47,6 +62,16 @@ module.exports = (db) => {
         .status(500)
         .json({ error: err.message });
     });
+  });
+
+  // render the "View Contributions" page for story_id
+  router.get("/view_contributions/:storyID", (req, res) => {
+    res.render("view_contributions");
+  });
+
+  // render the "Create Contribution" page for story_id
+  router.get("/create_contribution/:storyID", (req, res) => {
+    res.render("create_contribution");
   });
 
   // "Create Story" button on homepage
