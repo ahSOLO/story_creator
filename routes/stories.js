793b7008d2628eee44275f38700916a792eb9b99
Andrew Han <junmenghan@gmail.com>
2021-02-09 03:19:39 +0000

    update pg to latest version

--- routes/stories.js
+++ routes/stories.js
@@ -20,7 +20,33 @@ module.exports = (db) => {
 
   // render the "View Story" page for story_id
   router.get("/view/:storyID", (req, res) => {
-    res.render("view_story");
+    const queryString = `
+    SELECT stories.title as title, creators.name as creator, contributors.name as contributor, creator_animations.type as creator_animation, creator_photos.photo_url as creator_photo, stories.first_entry as first_entry, creator_sounds.type as creator_sound, contributions.order_rank as ord, contributor_animations.type as contributor_anim, contributor_photos as contributor_photo, contributions.content as entry, contributor_sounds.type as contributor_sound
+    FROM stories
+    JOIN contributions ON (story_id = stories.id AND contributions.status != 'pending')
+    JOIN users creators ON creator_id = creators.id
+    JOIN users contributors ON contributor_id = contributors.id
+    JOIN photos creator_photos ON creator_photos.id = stories.photo_id
+    JOIN photos contributor_photos ON contributor_photos.id = contributions.photo_id
+    JOIN animations creator_animations ON creator_animations.id = stories.animation_id
+    JOIN animations contributor_animations ON contributor_animations.id = contributions.animation_id
+    JOIN ambient_sounds creator_sounds ON creator_sounds.id = stories.ambient_sound_id
+    JOIN ambient_sounds contributor_sounds ON contributor_sounds.id = contributions.ambient_sound_id
+    WHERE stories.id = $1;
+    `
+    const queryParams = [req.params.storyID]
+
+    db.query(queryString, queryParams)
+    .then((data) => {
+      console.log(data.rows);
+      const templateVars = data.rows;
+      res.render("view_story", templateVars);
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({ error: err.message });
+    });
   });
 
   // "Create Story" button on homepage
