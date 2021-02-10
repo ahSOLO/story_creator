4caf5d5414c20e58f6718c03a8fad441e4588e41
Ryan Kang <rkang626@gmail.com>
2021-02-10 10:25:08 +0000

    remove text position from contributions table

--- db/schema/05_contributions.sql
+++ db/schema/05_contributions.sql
@@ -7,7 +7,6 @@ CREATE TABLE contributions (
   photo_id INTEGER REFERENCES photos(id) ON DELETE CASCADE,
   animation_id INTEGER REFERENCES animations(id) ON DELETE CASCADE,
   ambient_sound_id INTEGER REFERENCES ambient_sounds(id) ON DELETE CASCADE,
-  text_position VARCHAR(255) NOT NULL,
   content TEXT NOT NULL,
   order_rank INTEGER NOT NULL,
   status VARCHAR(255) NOT NULL DEFAULT 'pending',
